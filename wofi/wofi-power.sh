1aaf0b6218f2282ca2ac7b20c3d2c7bf7855cd14
yuchanns <me@yuchanns.xyz>
2022-08-06 17:05:41 +0800

    add wofi

---
+++ wofi/wofi-power.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+Shutdown_command="systemctl poweroff"
+Reboot_command="systemctl reboot"
+Logout_command="swaymsg exit"
+Hibernate_command="systemctl hibernate"
+Suspend_command="systemctl suspend"
+Back_command=""
+
+# you can customise the rofi command all you want ...
+rofi_command="rofi -theme /home/yuchanns/.config/wofi/launcherSmoll.rasi"
+options=$'Back\nShutdown\nLogout\nReboot\nHibernate\nSuspend' 
+
+# ... because the essential options (-dmenu and -p) are added here
+eval \$"$(echo "$options" | $rofi_command -dmenu -p "")_command"
