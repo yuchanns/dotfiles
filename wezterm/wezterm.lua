1f33fb4427fff93ce8bcbb6635ea1e18f758a67a
yuchanns <me@yuchanns.xyz>
2022-08-17 00:31:03 +0800

    update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,49 +1,59 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 wezterm.on("update-right-status", function(window, pane)
-    local date = "  "..wezterm.strftime("%a %b %-d %H:%M  ")
+	local date = "  " .. wezterm.strftime("%a %b %-d %H:%M  ")
 
-    window:set_right_status(wezterm.format({
-        { Attribute = { Underline = "Single" } },
-        { Attribute = { Italic = true } },
-        { Foreground = { Color = "Cyan" } },
-        { Text = date }
-    }))
+	window:set_right_status(wezterm.format({
+		{ Attribute = { Underline = "Single" } },
+		{ Attribute = { Italic = true } },
+		{ Foreground = { Color = "Cyan" } },
+		{ Text = date },
+	}))
 end)
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  if tab.is_active then
-    return {
-      { Foreground = { Color="Colour231" } },
-      { Background = { Color="#24283B" } },
-      { Attribute = { Italic = true } },
-      { Text = " ﲵ : "..tab.active_pane.title.." "},
-    }
-  end
-  return tab.actvie_pane.id..tab.active_pane.title
+	if tab.is_active then
+		return {
+			{ Foreground = { Color = "Colour231" } },
+			{ Background = { Color = "#24283B" } },
+			{ Attribute = { Italic = true } },
+			{ Text = " ﲵ : " .. tab.active_pane.title .. " " },
+		}
+	end
+	return tab.actvie_pane.id .. tab.active_pane.title
 end)
 
 return {
-    default_cwd = "/Users/yuchanns/Coding",
-    font = wezterm.font_with_fallback({ "JetBrains Mono", "MesloLGS NF", "Sarasa Mono SC Nerd" }),
-    font_size = 16.0,
+	default_cwd = "/home/yuchanns/Coding",
+	font_rules = {
+		{
+			italic = true,
+			font = wezterm.font("Operator Mono Medium", { italic = true }),
+		},
+	},
+	font = wezterm.font_with_fallback({ "JetBrains Mono", "Sarasa Mono SC Nerd" }),
+	font_size = 12.0,
 
-    window_background_opacity = 0.88,
-    color_scheme = "tokyonight",
-    window_padding = {
-        left = 0,
-        right = 0,
-        top = 0,
-        bottom = 0
-    },
-    foreground_text_hsb = {
-        hue = 1.0,
-        saturation = 1.0,
-    },
-    use_ime = true,
-    hide_tab_bar_if_only_one_tab = true,
-    window_frame = {
-        font_size = 14.0
-    },
-    term = "wezterm"
+	use_ime = true,
+	xim_im_name = "fcitx5",
+	enable_wayland = true,
+
+	window_background_opacity = 0.9,
+	color_scheme = "tokyonight",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	foreground_text_hsb = {
+		hue = 1.0,
+		saturation = 1.0,
+	},
+	use_ime = true,
+	hide_tab_bar_if_only_one_tab = true,
+	window_frame = {
+		font_size = 14.0,
+	},
+	term = "xterm-256color",
 }
