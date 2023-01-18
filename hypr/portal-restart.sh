b2ca4a1367b96d7436e6fecae4343ab05f0b782c
yuchanns <me@yuchanns.xyz>
2023-01-18 15:54:57 +0800

    Remove redundant script of hyprland

--- hypr/portal-restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-sleep 4
-killall xdg-desktop-portal-wlr
-killall xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-wlr &
-sleep 4
-/usr/lib/xdg-desktop-portal &
