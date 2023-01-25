66631b660fb40fa3cbf28f14ef8193f92e6c42bc
yuchanns <me@yuchanns.xyz>
2023-01-26 02:13:53 +0800

    Add swaylock

---
+++ hypr/scripts/idle-handle.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# https://github.com/swaywm/sway/issues/3631
+lockcmd="swaylock --indicator-idle-visible --indicator-radius 100 --indicator-thickness 7 --ignore-empty-password --ring-color 53E2AE --ring-ver-color 53E2AE --ring-wrong-color EE4F84 --key-hl-color EE4F84 --text-color ffffff --text-ver-color ffffff --text-wrong-color EE4F84 --line-color 00000000 --inside-color 00000088 --inside-ver-color 00000088 --inside-wrong-color 00000088 --separator-color 00000000"
+
+swayidle -w \
+  timeout 1200 "$lockcmd & if [[ \"$(cat /sys/class/power_supply/macsmc-battery/status)\" == \"Discharging\" ]]; then systemctl suspend; else hyprctl dispatch dpms off; fi" resume "hyprctl dispatch dpms on"\
+    before-sleep "$lockcmd"
+
--- hypr/scripts/launch-bar.sh
+++ hypr/scripts/launch-bar.sh
@@ -3,5 +3,4 @@
 swww init
 sleep 4
 swww clear 7AA2F7
-sleep 4
 ~/.config/eww/launch_bar
