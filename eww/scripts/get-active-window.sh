3b8b33b29fbec3b1533f0ae889221dcb82f33b27
yuchanns <me@yuchanns.xyz>
2023-06-07 14:49:59 +0800

    (eww): customized active-window name

--- eww/scripts/get-active-window.sh
+++ eww/scripts/get-active-window.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+declare -A dict=(["wezterm"]="wezterm" ["chromium"]="chrome")
+
 hyprctl activewindow -j | jq --raw-output .class
-socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | stdbuf -o0 grep '^activewindow>>' | stdbuf -o0 awk -F '>>|,' '{print $2}'
+socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | stdbuf -o0 grep '^activewindow>>' | stdbuf -o0 awk -F '>>|,' '{ if (index($2, "wezterm") != 0) { print "wezterm" } else { print $2 } }'
