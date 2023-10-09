ae238a686b2ebb9c3c03491889310e803d6d2bf8
yuchanns <me@yuchanns.xyz>
2023-10-10 00:38:51 +0800

    (hyprland): disable logging

--- hypr/scripts/launch-bar.sh
+++ hypr/scripts/launch-bar.sh
@@ -2,5 +2,6 @@
 
 swww init
 sleep 4
-swww img ~/.config/hypr/wallpapers/background_01.jpeg 
+# swww img ~/.config/hypr/wallpapers/background_01.jpeg 
+swww img ~/.config/hypr/wallpapers/background_02.jpg 
 ~/.config/eww/launch_bar
