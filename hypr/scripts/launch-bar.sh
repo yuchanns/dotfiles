70f40be3d907aaf34548baf6ec0b9a82c285c7ba
yuchanns <me@yuchanns.xyz>
2023-11-18 15:26:38 +0800

    (hypr): add new background

--- hypr/scripts/launch-bar.sh
+++ hypr/scripts/launch-bar.sh
@@ -3,5 +3,6 @@
 swww init
 sleep 4
 # swww img ~/.config/hypr/wallpapers/background_01.jpeg 
-swww img ~/.config/hypr/wallpapers/background_02.jpg 
+# swww img ~/.config/hypr/wallpapers/background_02.jpg 
+swww img ~/.config/hypr/wallpapers/background_03.jpg 
 ~/.config/eww/launch_bar
