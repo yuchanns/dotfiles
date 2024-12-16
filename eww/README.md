1f9586fd0a2e52c144210ea1ae4eb9ff7a2c2b63
Hanchin Hsieh <me@yuchanns.xyz>
2024-12-16 10:51:46 +0800

    (hypringo): add Hypringo

---
+++ eww/README.md
@@ -0,0 +1,1 @@
+Rely on [Hypringo](./Hypringo) for Hyprland change notification
--- hypr/scripts/redirect_log.sh
+++ hypr/scripts/redirect_log.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-LOG_PATH="/tmp/hypr/$(hyprctl instances -j | jq -r '.[0].instance')/hyprland.log"
+LOG_PATH="$XDG_RUNTIME_DIR/hypr/$(hyprctl instances -j | jq -r '.[0].instance')/hyprland.log"
 # cat $LOG_PATH
 rm $LOG_PATH && ln -s /dev/null $LOG_PATH
